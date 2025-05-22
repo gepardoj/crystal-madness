c26bf33c737f5bc5c12730291099a5c917bfacb0
gepardoj <anna.ra.phoenix@gmail.com>
2025-05-22 17:36:12 +0300

    rendering grid of hearts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,39 @@
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 import './style.css'
 import * as THREE from 'three';
 
 (() => {
+  const crystals: THREE.Mesh[] = [];
+
+  const OFFSET = .4 as const;
+  const GRID_SIZE = [5, 5] as const;
+
+  const loader = new GLTFLoader();
+  loader.load(
+    `/crystal.glb`,
+    (gltf) => {
+      const child = gltf.scene.children[0];
+      if (!(child instanceof THREE.Mesh)) return
+      for (let i = 0; i < GRID_SIZE[0] * GRID_SIZE[1]; i++) {
+        const crystal = new THREE.Mesh(child.geometry, child.material);
+        scene.add(crystal);
+        crystal.position.set(
+          i % GRID_SIZE[0] * OFFSET,
+          Math.floor(i / GRID_SIZE[0]) * -OFFSET,
+          0);
+        crystal.rotation.set(90, 0, 0);
+        crystal.scale.set(.05, .05, .05);
+        crystals.push(crystal);
+      }
+      console.log("resources loaded");
+    },
+    undefined,
+    () => {
+      console.log("error at loading resources")
+    }
+  )
+
+  console.log("init canvas");
   const canvas = document.querySelector("#game");
   if (!(canvas instanceof HTMLCanvasElement)) return;
 
@@ -11,20 +43,20 @@ import * as THREE from 'three';
   const renderer = new THREE.WebGLRenderer({
     canvas, antialias: true,
   });
+  renderer.setSize(canvas.clientWidth, canvas.clientHeight);
 
   const light = new THREE.DirectionalLight();
   light.position.set(1, 1, 1);
   scene.add(light);
 
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshStandardMaterial({ color: 0xff_33_33 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-  camera.position.z = 5;
+  camera.position.set(.8, -.8, 100)
 
   function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    crystals.forEach((crystal, i) => {
+      crystal.rotation.z += 0.01 * (i + 1);
+    })
+
+    // console.log(renderer.info);
     renderer.render(scene, camera);
   }
   renderer.setAnimationLoop(animate);
